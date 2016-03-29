8acd291712adbaabf1fd174eb076589c4dc0324f
Antonio Marino <antomarino93@gmail.com>
2016-03-29 15:20:53 +0200

    add files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame - guaranteed to run after all items processed in Update
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
